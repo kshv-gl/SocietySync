7aec4fd9cba5ce98398b3f739b630d765f2b53e5
lalit0505 <lalit10565249@gmail.com>
2026-05-08 00:14:33 +0530

    Added COLORS

--- societysync/src/components/ComplaintCard.jsx
+++ societysync/src/components/ComplaintCard.jsx
@@ -8,8 +8,13 @@ function ComplaintCard({ complaint }) {
       <div className="card-details">
         <span>Flat: {complaint.flatNo}</span>
 
-        <span className="department">
-          {complaint.department}
+        <span className={`department ${complaint.department}`}>
+        {complaint.department === "Plumbing" && "🔧"}
+        {complaint.department === "Electricity" && "⚡"}
+        {complaint.department === "Security" && "🛡️"}
+        {complaint.department === "Cleaning" && "🧹"}
+
+        {" "}{complaint.department}
         </span>
       </div>
 
